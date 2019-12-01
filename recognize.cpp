00875dbf81aa110aab59a430fa86c24ae105084f
Willy-Huang101 <57385377+Willy-Huang101@users.noreply.github.com>
2019-12-01 19:56:28 +0800

    Update recognize.cpp
    此版本可測試正確率or自己測試自己寫的字 修改測試方法:修改process.h中之define參數。

--- recognize.cpp
+++ recognize.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
-#include <queue>
-#include <math.h>
 #include "process.h"
 
 using namespace std;
 using namespace cv;
 
+void Test(Num_data data[], int train_data_count);
+
 Num_data data[100000];
 int main()
 {
 	int train_data_count;
+
 	train_data_count = read_train_data(data);
 
 	cout << "Read train data = " << train_data_count << endl;
 
+	Test(data, train_data_count);
+
 	// Just test for one picture.
-	char pathname[20] = {};
+	/*char pathname[20] = {};
+
 	sprintf(pathname, "./test/%d.jpg", 1);
 
 	Mat image = imread(pathname, 0);
@@ -26,12 +29,17 @@ int main()
 		return -1;
 	}
 
-
 	image = process(image);
 
-	//print_img(image);
+	print_img(image);
 
-	cout << "Predict number is : "  << predict(image, data, train_data_count) << endl;
+	int number = predict(image, data, train_data_count);
+
+	cout << "Predict number is : "  << number << endl;
+
+	sprintf(pathname, "./predict/%d.jpg", number);
+
+	image = imread(pathname, 0);
 
   	// 建立視窗
   	namedWindow("Display Image", WINDOW_AUTOSIZE);
@@ -40,7 +48,37 @@ int main()
   	imshow("Display Image", image);
 
   	// 顯示視窗，直到任何鍵盤輸入後才離開
-  	waitKey(0);
+  	waitKey(0);*/
 
 	return 0;
+}
+
+void Test(Num_data data[], int train_data_count)
+{
+	Mat image;
+	int total = 0;
+	int error = 0;
+
+	for (int n = 0; n <= 9; n++) {
+		int count = 0;
+		int num_err = 0;
+		for (int i = 4000; count != test_pictures_per_number; i++) {
+			char pathname[20] = {};
+			sprintf(pathname, "./test/%d_%d.jpg", n, i);
+			image = imread(pathname, 0);
+			if (image.data) {
+				total++;
+				count++;
+				image = process(image);
+				int predict_num = predict(image, data, train_data_count);
+				if (predict_num != n) {
+					error++;
+					num_err++;
+				}
+			}
+		}
+		printf("%d error times = %d\n", n, num_err);
+	}
+	printf("Total = %d error times = %d\n", total, error);
+	printf("Error rate = %.4f\n", float(error) / total);
 }
